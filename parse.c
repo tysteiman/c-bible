0072ff6dbd6bdc063b6c97e741c65647a55841d6
Tyler Steiman <tsteiman@switchboxinc.com>
2019-10-08 19:53:44 -0400

    break things out into funs

--- parse.c
+++ parse.c
@@ -19,10 +19,52 @@ void parse(char *search)
 
     while(fgets(line, VERSE_LEN_MAX, fp))
     {
-        if (strcasestr(line, search))
-        {
-            printf("%s", line);
-        }
+        processLine(line, search);
     }
 }
 
+void processLine(char *line, char *search)
+{
+    char *find = strcasestr(line, search);
+
+    long findpos = find - line;
+    int findlen  = strlen(search);
+    int linelen = strlen(line);
+
+    if (find)
+    {
+        printLine(findpos, line, findlen, linelen);
+    }
+}
+
+void printLine(long findpos, char *line, int findlen, int linelen)
+{
+    int i = 0;
+
+    // print everything before the match
+    for (; i < findpos; ++i)
+    {
+        printf("%c", line[i]);
+    }
+
+    int f = 0;
+
+    // start color output for match
+    COLOR();
+
+    // print match (will be in color)
+    for (; f < findlen; ++f)
+    {
+        printf("%c", line[i]);
+        ++i;
+    }
+
+    // stop color for rest of line
+    CLEAR();
+
+    // print rest of line
+    for (; i < linelen; ++i)
+    {
+        printf("%c", line[i]);
+    }
+}
--- parse.h
+++ parse.h
@@ -2,7 +2,14 @@
 #define BIBLE_PARSE_H
 
 #define VERSE_LEN_MAX 500
+#define MAIN_COLOR "\033[1;31m"
+#define RESET_COLOR "\033[0m"
+
+#define COLOR()(printf("%s", MAIN_COLOR))
+#define CLEAR()(printf("%s", RESET_COLOR))
 
 void parse(char *search);
+void processLine(char *line, char *search);
+void printLine(long findpos, char *line, int findlen, int linelen);
 
 #endif
